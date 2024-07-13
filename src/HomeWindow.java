7ef195d1278c3388fb29dfe9684267d4725ea07f
Pollard Samba <69053974+POLLARD1145@users.noreply.github.com>
2024-07-13 17:49:40 +0200

    ADDED HOME WINDOW

--- src/HomeWindow.java
+++ src/HomeWindow.java
@@ -1,5 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class HomeWindow extends JFrame {
-    
+public class HomeWindow{
+    JFrame mainFrame = new JFrame();
+    JPanel mainBox = new JPanel();
+    JPanel messageBox = new JPanel();
+    JLabel message = new JLabel("Welcome to Zambia Trivia Quiz");
+    public HomeWindow(){
+        mainFrame.setTitle("Home - Zambia Trivia");
+        mainFrame.setSize(1200,800);
+        mainFrame.add(mainBox, BorderLayout.CENTER);
+        
+
+        //end
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+    }
 }
